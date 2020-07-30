57b1dae212b83d7553896f147e00effa56783145
PatrickCoakley23 <patrickcoakley23@gmail.com>
2020-07-30 08:59:53 +0000

    added checkout to web app

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -1,5 +1,4 @@
 """boutique_ado URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
@@ -18,11 +17,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- checkout/forms.py
+++ checkout/forms.py
@@ -1,16 +1,16 @@
 from django import forms
-from .models import Order 
+from .models import Order
 
 
 class OrderForm(forms.ModelForm):
-    class Meta: 
+    class Meta:
         model = Order
         fields = ('full_name', 'email', 'phone_number',
                   'street_address1', 'street_address2',
                   'town_or_city', 'postcode', 'country',
                   'county',)
 
- def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """
         Add placeholders and classes, remove auto-generated
         labels and set autofocus on first field
